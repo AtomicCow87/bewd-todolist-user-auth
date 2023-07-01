d15a3c20a492d679729225fe5844a264f503ca2b
Zach <zkdoran@gmail.com>
2023-07-01 15:06:15 -0400

    adduseridtosessions and migrated

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_07_01_190312) do
+ActiveRecord::Schema.define(version: 2023_07_01_190441) do
 
   create_table "sessions", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_sessions_on_user_id"
   end
 
   create_table "tasks", force: :cascade do |t|
