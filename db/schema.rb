0e810614bc436b646762dbcfe72153c866bbad9e
Zach <zkdoran@gmail.com>
2023-07-01 14:21:31 -0400

    undo schema change

--- db/schema.rb
+++ db/schema.rb
@@ -17,12 +17,4 @@ ActiveRecord::Schema.define(version: 2016_12_15_094136) do
     t.string 'content'
     t.boolean 'completed', default: false
   end
-
-ActiveRecord::Schema.define(version: 2016_12_15_094136) do
-  create_table 'users', force: :cascade do |t|
-    t.string 'username'
-    t.string 'password'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-  end
 end
