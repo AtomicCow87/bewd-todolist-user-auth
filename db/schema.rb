244eda290238ce3d8c190c4a7eda75eb7fa333b0
Zach <zkdoran@gmail.com>
2023-07-01 15:03:57 -0400

    create sessions, migrate table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_07_01_185055) do
+ActiveRecord::Schema.define(version: 2023_07_01_190312) do
+
+  create_table "sessions", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "tasks", force: :cascade do |t|
     t.datetime "created_at", null: false
